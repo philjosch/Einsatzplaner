754e9e9dd083e7493f6113cf272ac3ffe828be9c
philjosch <github@philipp-schepper.de>
2017-09-30 01:37:49 +0200

    Kommentare eingefügt
    - Zu den verschiedenen Fehlern wurden Kommentare eingefügt.
    - Ergänzung um den Fehler „ExternOk“

--- Programm/enums.h
+++ Programm/enums.h
@@ -5,4 +5,9 @@
 
 
 enum Category { Tf, Tb, Zf, Service, Zub, Begleiter, Buero, Werkstatt, ZugVorbereiten, Sonstiges=100 };
-enum Misstake { OK, PersonNichtGefunden, FalscheQualifikation, SonstigerFehler};
+enum Mistake { OK, // Returned if the person was found in the system and is abled to do the job
+                PersonNichtGefunden, // If the person has not been found and there is no reason to interpret it as an external
+                FalscheQualifikation, // registerd person was found but is not qualified to do the job
+                ExternOk, // The person was not found in the system, but there is a reason to interpret it as an external
+                SonstigerFehler // Some other misstake during the process -> check the inputs
+              };
